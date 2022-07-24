14000cf1988718aa023dd953e64a064a5cf79902
kenshirou01 <rush2065kingcrab0120@gmail.com>
2022-07-25 04:46:03 +0900

    Add Component or function

---
+++ src/components/inputTodo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const inputArea = {
+  backgroundColor: '#c1ffff',
+  width: '400px',
+  height: '30px',
+  borderRadius: '8px',
+  padding: '8px',
+  margin: '8px',
+}
+
+export const InputTodo = (props) => {
+  const { todoText, onChange, onClick } = props;
+  return (
+    <div style={inputArea}>
+      <input
+        placeholder='TODOを入力'
+        value={todoText}
+        onChange={onChange}
+      />
+      <button onClick={onClick}>追加</button>
+    </div>
+  );
+};
